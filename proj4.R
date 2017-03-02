5a12a5da7b943b5d55dae030540e3c6b6a7864e9
Ajit Dingankar <Ajit.Dingankar@ieee.org>
2017-03-01 21:11:51 -0800

    Completed proj4 and fixed proj2 cosmetically

---
+++ proj4.R
@@ -0,0 +1,33 @@
+# On Linux, run with 
+# Rscript proj4.R
+
+source("read.R")
+
+metering_colors = c("black", "red", "blue")
+
+dev = png(filename = "plot4.png", width = 480, height = 480, units = "px")
+
+par(mfrow=c(2,2))
+# Plot 1,1
+plot(rawData$Time, rawData$Global_active_power, type = "l"
+     , xlab = "", ylab = "Global Active Power")
+
+# Plot 1,2
+plot(rawData$Time, rawData$Voltage, type = "l"
+     , xlab = "datetime", ylab = "Voltage")
+
+# Plot 2,1
+plot(rawData$Time, rawData$Sub_metering_1, col = metering_colors[1]
+     , type = "c", xlab = "", ylab = "Energy sub metering")
+lines(rawData$Time, rawData$Sub_metering_2, col = metering_colors[2]
+      , xaxt = "n", yaxt = "n")
+lines(rawData$Time, rawData$Sub_metering_3, col = metering_colors[3]
+      , xaxt = "n", yaxt = "n")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3") 
+       , col = metering_colors, lwd = 1, bty = "n")
+
+# Plot 2,2
+plot(rawData$Time, rawData$Global_reactive_power, type = "l"
+     , xlab = "datetime", ylab = "Global_reactive_power")
+
+dev.off()
